076d3a99d0a84d98514fa8d1b8bd2ff93bf1a623
gretchenmarie <gretchenmward@gmail.com>
2018-07-11 10:49:40 -0500

    added css and JS

---
+++ index.js
@@ -0,0 +1,10 @@
+var btnClicked = function(){
+  
+    var name = $("name").val
+    var lastname = $ ("last-name").val
+    var Email = $("email").val
+    var message = $("message").val
+    console.log("User input email:"),
+    console.log("User input message:"),
+     console.log("Thanks for reaching out!");           
+  }
